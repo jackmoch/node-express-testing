784c3ef8c965e6f0b126f25c4d616642661a470a
Jack Mocherman <mochermanjack@gmail.com>
2016-10-17 11:13:50 -0500

    response now sends correct message

--- server.js
+++ server.js
@@ -3,6 +3,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello'))
+app.get('/', (req, res) => res.send('Hello World'))
 
 module.exports = app
